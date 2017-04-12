4f9fc2d157fb19a7fb793f23cbbafac0a6987c4d
SenpaiRar <windowsbeb@gmail.com>
2017-04-12 00:55:01 -0400

    Added a check so players can't spam missiles

--- Assets/Scripts/Rewritten Code/Player.cs
+++ Assets/Scripts/Rewritten Code/Player.cs
@@ -4,25 +4,37 @@ using System.Collections;
 public class Player : MonoBehaviour {
     public delegate void LaunchDelegate();
     public event LaunchDelegate LaunchEvent;
-
+    
     public Vector3 missileTarget;
     public Transform missileOrigin;
     public GameObject missile;
     public Camera mainCamera;
-    public static bool isMissleFired;
-	
+    private bool canShoot;
+    private int amountOfMissilesFired;
+    public int AmountOfMissileCanShoot; //How many missiles does the play have before reloading
 	void Start () {
-        isMissleFired = false;
-	}
+        amountOfMissilesFired = 0;
+        canShoot = true;
+    }
 	
-	// Update is called once per frame
+	
 	void Update () {
-        if (Input.GetMouseButtonDown(0) && !isMissleFired) //when the user hits the left mouse button
+        if (Input.GetMouseButtonDown(0) && canShoot) //when the user hits the left mouse button
         {
-            SetTarget(); 
-            
-            LaunchMissile(missile.GetComponent<Missile>());
+            if (amountOfMissilesFired < 3)
+            {
+                amountOfMissilesFired += 1;
+                SetTarget();
+                LaunchMissile(missile.GetComponent<Missile>());
+            }
+            else
+            {
+                canShoot = false;
+                StartCoroutine(Reload());
+            }
+         
         }
+        
     }
 
     void LaunchMissile (Missile Projectile)
@@ -42,5 +54,14 @@ public class Player : MonoBehaviour {
         missileTarget.x = mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
         missileTarget.y = mainCamera.ScreenToWorldPoint(Input.mousePosition).y;
         missileTarget.z = 0;
+    }
+    
+    IEnumerator Reload()
+    {
+        Debug.Log("Reload!");
+        yield return new WaitForSeconds(2);
+        canShoot = true;
+        amountOfMissilesFired = 0;
+        yield break;
     }
 }
